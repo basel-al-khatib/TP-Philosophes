17d7073fbaff7e0d0dc3c6dea6b767fdabab370e
balkhati <basel.al_khatib@etud.univ-jfc.fr>
2024-03-15 15:54:40 +0100

    TP-philosophers

--- src/main/java/diningphilosophers/ChopStick.java
+++ src/main/java/diningphilosophers/ChopStick.java
@@ -1,33 +1,45 @@
 package diningphilosophers;
 
-public class ChopStick {
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-    private static int stickCount = 0;
+public class ChopStick {
+    private final Lock lock = new ReentrantLock();
+    private final Condition conditionA = lock.newCondition();
+
+    private static int counter = 0;
     private boolean iAmFree = true;
     private final int myNumber;
 
     public ChopStick() {
-        myNumber = ++stickCount;
+        myNumber = ++counter;
     }
 
-    synchronized public boolean tryTake(int delay) throws InterruptedException {
-        if (!iAmFree) {
-            wait(delay);
-            if (!iAmFree) // Toujours pas libre, on abandonne
-            {
+     public boolean tryTake(int delay) throws InterruptedException {
+        lock.lock();
+       try { while (!iAmFree) {
+           conditionA.await();
                 return false; // Echec
-            }
         }
         iAmFree = false;
         // Pas utile de faire notifyAll ici, personne n'attend qu'elle soit occupée
         return true; // Succès
     }
+    finally {lock.unlock();}}
 
-    synchronized public void release() {
+
+      public void release() {
+        lock.lock();
+        try {
         iAmFree = true;
-        notifyAll();
+        conditionA.signalAll();
         System.out.println("Stick " + myNumber + " Released");
-    }
+    }finally {
+            lock.unlock();
+        }
+      }
+
 
     @Override
     public String toString() {
